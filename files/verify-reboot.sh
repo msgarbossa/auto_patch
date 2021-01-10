3e999ebc408b3dc5c0785f58d708afa3b7797ede
Mike S <mikeysky@gmail.com>
2021-01-10 00:28:14 -0700

    fix verify on boot

--- files/verify-reboot.sh
+++ files/verify-reboot.sh
@@ -14,9 +14,9 @@ start() {
     RETVAL=1; return 1
   fi
 
-  # Find time of last patching in epoch seconds from patching log file
+  # Find time of last commands in epoch seconds from pre-patching
   patch_secs=0
-  patch_file="/var/log/auto-patch/current/auto-patch-update.out"
+  patch_file="/var/log/auto-patch/current/cmds.json"
   if [ -e $patch_file ]; then
     patch_sec_current=$(stat -c "%Y" $patch_file)
     [ $patch_sec_current -gt $patch_secs ] && patch_secs=$patch_sec_current
@@ -27,10 +27,12 @@ start() {
   report_file="/var/log/auto-patch/current/report.json"
   if [ -e $report_file ]; then
     verify_secs=$(stat -c "%Y" $report_file)
+  else
+    do_verify=1
   fi
 
   # do verification if last verify was done before patching
-  [ $verify_secs -lt $patch_secs ] && do_verify=1
+  [ $verify_secs -le $patch_secs ] && do_verify=1
 
   if [ $do_verify != 1 ]; then
     echo "Verification has already been done since last patching.  Verification will not continue." | tee -a $LOG
